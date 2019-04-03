eedc8a4c87e74f58fb800e0ac6cb074c230317c2
Alex <ambergeron@umass.edu>
2019-04-03 16:55:51 -0400

    Added basic controller file

---
+++ controllers/FlightController.jsx
@@ -0,0 +1,19 @@
+import express from 'express';
+import db from './db/db';
+// Set up the express app
+
+const app = express();
+
+// get all todos
+app.get('/flights', (req, res) => {
+  res.status(200).send({
+    success: 'true',
+    message: 'todos retrieved successfully',
+    todos: db
+  })
+});
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+});
