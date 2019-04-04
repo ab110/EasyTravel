c20f5c4ee890783bdbe39c1569e73f4a3d812b05
Alex <ambergeron@umass.edu>
2019-04-04 16:26:39 -0400

    Integrated database with backend

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
+const mongoose = require("mongoose");
+var cors = require('cors');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+
+const API_PORT = 3001;
+app.use(cors());
+const router = express.Router();
+
+// this is our MongoDB database
+const dbRoute = "mongodb+srv://root:alpine@easytravel-xd6sb.mongodb.net/test?retryWrites=true";
+
+// connects our back end code with the database
+mongoose.connect(
+  dbRoute,
+  { useNewUrlParser: true }
+);
+
+let db = mongoose.connection;
+
+db.once("open", () => console.log("connected to the database"));
+
+// checks if connection with the database is successful
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET route
 app.get('/express_backend', (req, res) => {
+  console.log("yoo");
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
